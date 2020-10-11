9c6cacd90a546055bfc2c09e133d4774502a28ea
Jeremy Swartwood <jeremy.swartwood@gmail.com>
2020-10-11 14:28:46 -0800

    Refactor to use methods

--- main.py
+++ main.py
@@ -2,14 +2,26 @@
 # Loops are used instead of mapping, list comprehension, or lambda
 # Loops should be "fast enough" for us for this example
 
-command_file = open(r"commands.txt","r")
-command_list_input = command_file.readlines()
+# Load file: Load a file and return the commands
+def loadfile(filename):
+    # Open the file and read all lines into a list
+    command_file = open(filename, "r")
+    command_list_input = command_file.readlines()
 
-command_list = []
-# Convert all commands to lowercase, remove leading and trailing white space, and then add to command_list
-for command in command_list_input:
-    command_list.append(command.lower().strip())
+    # Convert all commands to lowercase, remove leading and trailing white space, and then add to command_list
+    command_list = []
+    for command in command_list_input:
+        command_list.append(command.lower().strip())
 
-print(command_list)
+    print(command_list)
+    command_file.close()
 
-command_file.close()
+    return command_list
+
+
+def main():
+    command_list = loadfile("commands.txt")
+
+
+if __name__ == "__main__":
+    main()
